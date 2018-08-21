9128bbbdc965d8c10e13ca889ce507183d6862ff
dehasi <rgaleyev@gmail.com>
2018-08-21 15:41:38 +0200

    PICNIC: Refactor solution

--- src/main/java/org/interview/start/Runner.java
+++ src/main/java/org/interview/start/Runner.java
@@ -7,7 +7,6 @@ import org.interview.oauth.twitter.TwitterAuthenticationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -16,15 +15,15 @@ import java.util.stream.Collectors;
 public class Runner {
     private static final Logger LOGGER = LoggerFactory.getLogger(Runner.class);
 
-    public static void main(String[] args) throws TwitterAuthenticationException, IOException {
+    public static void main(String[] args) throws TwitterAuthenticationException {
         TwitterClient gateway = new TwitterClient();
 
-        Map<User, List<Tweet>> userTweets2 = gateway.streamTweetsByWord("bieber").collect(Collectors.groupingBy(tweet -> tweet.user));
-        userTweets2.values().forEach(tweets -> tweets.sort(Comparator.comparing(t -> t.createdAt)));
-        userTweets2.keySet().stream()
+        Map<User, List<Tweet>> userTweets = gateway.streamTweetsByWord("bieber").collect(Collectors.groupingBy(tweet -> tweet.user));
+        userTweets.values().forEach(tweets -> tweets.sort(Comparator.comparing(t -> t.createdAt)));
+        userTweets.keySet().stream()
                 .sorted(Comparator.comparing(user -> user.createdAt))
                 .forEach(user ->
-                        LOGGER.info("User {} has tweets {}", user, userTweets2.get(user))
+                        LOGGER.info("User {} has tweets {}", user, userTweets.get(user))
                 );
     }
 }
